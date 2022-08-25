24889a7d24b62da7cac91746bad1c564ccb7d7c2
SHANOOR AHMED <76896805+shanoorahmed@users.noreply.github.com>
2022-08-25 08:00:45 +0530

    Create waterjug.py

---
+++ waterjug.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+def BFS(a, b, target):
+	m = {}
+	isSolvable = False
+	path = []
+	q = deque()
+	q.append((0, 0))
+	while (len(q) > 0):
+		u = q.popleft()
+		if ((u[0], u[1]) in m):
+			continue
+		if ((u[0] > a or u[1] > b or u[0] < 0 or u[1] < 0)):
+			continue
+		path.append([u[0], u[1]])
+		m[(u[0], u[1])] = 1
+		if (u[0] == target or u[1] == target):
+			isSolvable = True
+			if (u[0] == target):
+				if (u[1] != 0):
+					path.append([u[0], 0])
+			else:
+				if (u[0] != 0):
+					path.append([0, u[1]])
+			break
+		q.append([u[0], b])
+		q.append([a, u[1]])
+		q.append([u[0], 0])
+		q.append([0, u[1]])
+		for ap in range(max(a, b) + 1):
+			c = u[0] + ap
+			d = u[1] - ap
+			if (c <= a and d >= 0):
+				q.append([c, d])
+			c = u[0] - ap
+			d = u[1] + ap
+			if (c >= 0 and d <= b):
+				q.append([c, d])
+	if (not isSolvable):
+		print("No solution")
+	else:
+	    sz = len(path)
+	    for i in range(sz):
+	        print(path[i])
+
+if __name__ == '__main__':
+	Jug1, Jug2, target = 10, 7, 6
+	BFS(Jug1, Jug2, target)
+	
